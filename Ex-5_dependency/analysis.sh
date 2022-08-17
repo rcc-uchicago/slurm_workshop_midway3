9ad3b5d289d1b05072e2dd135250f17a0466f028
samaddar <dsamaddar@alaska.edu>
2022-08-17 17:12:55 -0500

    Adding exercises for midway3

---
+++ Ex-5_dependency/analysis.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --partition=caslake
+#SBATCH --account=rcc-staff
+#SBATCH --time=00:05:00
+#SBATCH --job-name=analysis
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.err
+#SBATCH --mem=1G
+#SBATCH --nodes=1
+
+# Run a command
+echo "Lots of lovely science"
+echo "Cutting edge research"
+echo "Algorithms"
---
+++ Ex-5_dependency/postprocessing.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --partition=caslake
+#SBATCH --account=rcc-staff
+#SBATCH --time=00:05:00
+#SBATCH --mem=1G
+#SBATCH --job-name=postprocessing
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.err
+#SBATCH --nodes=1
+
+# Run a command
+echo “Doing some postprocessing”
